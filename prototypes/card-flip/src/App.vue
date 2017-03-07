2b74973c7e3c8d145c8c8610c34b39f1d74a4839
chris-schmitz <schmitz.chris@gmail.com>
2017-03-07 09:41:13 -0600

    Moving wireframe and workflow from google drive to github

--- prototypes/card-flip/src/App.vue
+++ prototypes/card-flip/src/App.vue
@@ -5,8 +5,18 @@
         <li>faceUpCount: {{faceUpCount}}</li>
         <li>notification: {{notification}}</li>
       </ul>
+      <!--
+        note that we are intensionally letting the size of the notification component
+        show in the browser even if nothing is displayed (i.e. we're not using `v-if` 
+        or `v-show` directive to prevent the notification component from rendering). 
+        This is because we don't want the game board to jump up and down anytime a 
+        notification appears or disappears. Since we're using a slot and a prop to
+        show the color and contents of the notification we control whether or not the 
+        user sees the notification by providing or not providing the information necessary 
+        for the notification to show.
+      -->
       <notification :type="notification.type">
-        {{notification.message}}
+        <div>{{notification.message}}</div>
       </notification>
       <div class="game-board">
         <card v-for="card in cards" :card-data="card" :key="card.id"></card>
