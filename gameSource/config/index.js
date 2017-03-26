4195fcac1f6b627a01be1a2e02779d78ffe9cdc3
chris-schmitz <schmitz.chris@gmail.com>
2017-03-26 14:14:32 -0500

    redirects output path to the electron build folder

--- gameSource/config/index.js
+++ gameSource/config/index.js
@@ -4,8 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../electron-version/app/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../electron-version/app'),
+    // index: path.resolve(__dirname, '../dist/index.html'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
