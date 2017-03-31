838e7fc201ce2fd0c465782ed5adcd1cff21b4fa
chris-schmitz <schmitz.chris@gmail.com>
2017-03-31 17:11:43 -0500

    WIP: Adding in a bunch of hacky changes while trying to build to electron

--- gameSource/build/build.js
+++ gameSource/build/build.js
@@ -9,13 +9,23 @@ var chalk = require('chalk')
 var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
+var electronWebpackConfig = require('./webpack.electron-prod.conf')
 
 var spinner = ora('building for production...')
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
-  webpack(webpackConfig, function (err, stats) {
+
+  var configToUse
+  if (process.env.BUILD_DESTINATION === 'electron') {
+    configToUse = electronWebpackConfig
+  } else {
+    configToUse = webpackConfig
+  }
+
+
+  webpack(configToUse, function (err, stats) {
     spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
