46718b47f47048f1ccc3becc2a7cf26839d0016e
chris-schmitz <schmitz.chris@gmail.com>
2017-04-12 09:08:07 -0500

    Adds an app config and buildAndPack scripts specifically for pi version

--- game_source/src/app-config.js
+++ game_source/src/app-config.js
@@ -1,3 +1,8 @@
+// come back and build in some logic for setting the keypad value
+// from the build scripts so you don't have to remember to do it manually
+// at buildAndPack time
+
+
 const config = {
     showKeypad: false
 }
